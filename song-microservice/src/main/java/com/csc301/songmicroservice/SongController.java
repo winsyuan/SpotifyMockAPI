090593ac027129df1673a768d032e9d33cc09648
Aum Patel <aumpatel@Aums-MacBook-Pro.local>
2020-12-04 13:35:10 -0500

    Added comments and JavaDocs to song microservice

--- song-microservice/src/main/java/com/csc301/songmicroservice/SongController.java
+++ song-microservice/src/main/java/com/csc301/songmicroservice/SongController.java
@@ -40,7 +40,12 @@ public class SongController {
 		this.songDal = songDal;
 	}
 
-	
+	/**
+	 * Gets the song by specifying its id
+	 * @param songId - id for the song to get
+	 * @param request - GET request 
+	 * @return response with song data and OK status, or error status
+	 */
 	@RequestMapping(value = "/getSongById/{songId}", method = RequestMethod.GET)
 	public @ResponseBody Map<String, Object> getSongById(@PathVariable("songId") String songId,
 			HttpServletRequest request) {
@@ -48,15 +53,23 @@ public class SongController {
 		Map<String, Object> response = new HashMap<String, Object>();
 		response.put("path", String.format("GET %s", Utils.getUrl(request)));
 
+		//Calls DB operation
 		DbQueryStatus dbQueryStatus = songDal.findSongById(songId);
 
+		//Sets response 
 		response.put("message", dbQueryStatus.getMessage());
 		response = Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
 
+		//Sends response 
 		return response;
 	}
 
-	
+	/**
+	 * Get name of song by specifying its id
+	 * @param songId - id for the song to get title for 
+	 * @param request - GET request 
+	 * @return response with the song's title name and OK status, or error status
+	 */
 	@RequestMapping(value = "/getSongTitleById/{songId}", method = RequestMethod.GET)
 	public @ResponseBody Map<String, Object> getSongTitleById(@PathVariable("songId") String songId,
 			HttpServletRequest request) {
@@ -64,15 +77,23 @@ public class SongController {
 		Map<String, Object> response = new HashMap<String, Object>();
 		response.put("path", String.format("GET %s", Utils.getUrl(request)));
 		
+		//Calls DB operation
 		DbQueryStatus dbQueryStatus = songDal.getSongTitleById(songId); 
 		
+		//Sets response
 		response = Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
 		
+		//Sends response
 		return response; 
 
 	}
 
-	
+	/**
+	 * Deletes song which has the given id
+	 * @param songId - id for the song that is requested to be deleted
+	 * @param request - DELETE request 
+	 * @return OK status if deleted or error status if error occurred 
+	 */
 	@RequestMapping(value = "/deleteSongById/{songId}", method = RequestMethod.DELETE)
 	public @ResponseBody Map<String, Object> deleteSongById(@PathVariable("songId") String songId,
 			HttpServletRequest request) {
@@ -80,29 +101,38 @@ public class SongController {
 		Map<String, Object> response = new HashMap<String, Object>();
 		response.put("path", String.format("DELETE %s", Utils.getUrl(request)));
 		
+		//Request to send the profile microservice to delete from users favorite list
 		Request req = new Request.Builder().url("http://localhost:3002/deleteAllSongsFromDb/"+songId).put(RequestBody.create("", MediaType.parse("application/json; charset=utf-8"))).build(); 
-		DbQueryStatus dbQueryStatus; 
+		DbQueryStatus dbQueryStatus; //Status to send back
 		
 		try {
 		  Response res = client.newCall(req).execute(); 
 		  if(!res.isSuccessful()) {
-		    throw new Exception(); 
+		    throw new Exception(); //Error sending request
 		  }
 		  JSONObject ret = new JSONObject(res.body().string()); 
 		  if(!ret.getString("status").equals("OK")) {
-		    throw new Exception(); 
+		    throw new Exception(); //Error in profiles service
 		  }
+		  //Call DB operation to delete
 		  dbQueryStatus = songDal.deleteSongById(songId); 
 		  
 		} catch(Exception e) {
+		  //Error status since error occurred
 		  dbQueryStatus = new DbQueryStatus("Error in deleting from favourite lists", DbQueryExecResult.QUERY_ERROR_GENERIC);
 		}
-		
+		//Set response
 		response = Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
+		//Send response 
 		return response;
 	}
 
-	
+	/**
+	 * Adds the song with the given name, artist and album to database
+	 * @param params - Map of query parameters sent with request specifies the songs name, artist, and album
+	 * @param request - POST request
+	 * @return response with song that was added with its id and OK status or error status if something went wrong 
+	 */
 	@RequestMapping(value = "/addSong", method = RequestMethod.POST)
 	public @ResponseBody Map<String, Object> addSong(@RequestParam Map<String, String> params,
 			HttpServletRequest request) {
@@ -113,20 +143,30 @@ public class SongController {
 		String name = params.get(Song.KEY_SONG_NAME);
 		String artist = params.get(Song.KEY_SONG_ARTIST_FULL_NAME); 
 		String album = params.get(Song.KEY_SONG_ALBUM);
-		DbQueryStatus dbQueryStatus; 
+		DbQueryStatus dbQueryStatus; //Status to send back
 		
+		//Missing query parameters 
 		if(name == null || artist == null || album == null) {
 		  dbQueryStatus = new DbQueryStatus("Missing parameters for song", DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
 		} else {
+		  //Creates song object to store
 		  Song songToAdd = new Song(name, artist, album); 
+		  //Calls DB operation
 		  dbQueryStatus = songDal.addSong(songToAdd); 
 		}
+		//Sets response 
 		response = Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
 
 		return response;
 	}
 
-	
+	/**
+	 * Updates the song with the given sonfId by incrementing or decrementing like counter by 1 
+	 * @param songId - id of the song to have its likes updated
+	 * @param shouldDecrement - true specifies to decrement by 1 and false to increment by 1
+	 * @param request - PUT request 
+	 * @return OK status if successfully updated or error status if error occurred 
+	 */
 	@RequestMapping(value = "/updateSongFavouritesCount/{songId}", method = RequestMethod.PUT)
 	public @ResponseBody Map<String, Object> updateFavouritesCount(@PathVariable("songId") String songId,
 			@RequestParam("shouldDecrement") String shouldDecrement, HttpServletRequest request) {
@@ -134,12 +174,16 @@ public class SongController {
 		Map<String, Object> response = new HashMap<String, Object>();
 		response.put("data", String.format("PUT %s", Utils.getUrl(request)));
 		
-		DbQueryStatus dbQueryStatus;
+		DbQueryStatus dbQueryStatus; //Status to send back
+		
+		//Checks if query parameters are valid to convert to boolean 
 		if(shouldDecrement == null || (!Boolean.parseBoolean(shouldDecrement) && !shouldDecrement.contains("false"))) {
 		  dbQueryStatus = new DbQueryStatus("Must be true or false", DbQueryExecResult.QUERY_ERROR_GENERIC); 
 		} else {
+		  //Calls DB operation 
 		  dbQueryStatus = songDal.updateSongFavouritesCount(songId, Boolean.parseBoolean(shouldDecrement)); 
 		}
+		//Sets response 
         response = Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
 
         return response;
--- song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
+++ song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
@@ -20,7 +20,12 @@ public class SongDalImpl implements SongDal {
 	public SongDalImpl(MongoTemplate mongoTemplate) {
 		this.db = mongoTemplate;
 	}
-
+	/**
+	 * Adds song to mongoDB
+	 * @param songToAdd - The song to be inserted into mongo database
+	 * @return DbQueryStatus object that specifies the status of the operation 
+	 * and this object's data field may be set if the operation has response data
+	 */
 	@Override
 	public DbQueryStatus addSong(Song songToAdd) {
 		// TODO Auto-generated method stub
@@ -33,93 +38,155 @@ public class SongDalImpl implements SongDal {
          * Song.class)) { return new DbQueryStatus("Song Already Exist",
          * DbQueryExecResult.QUERY_ERROR_GENERIC); }
          */
+	  
+	    //Holds response data
 		Map<String, String> data = new HashMap<>(); 
+		//Song to add
 		Song insertedSong = this.db.insert(songToAdd);
+		//Adds the keys and values to data map
 		data.put(Song.KEY_SONG_NAME, songToAdd.getSongName()); 
 		data.put(Song.KEY_SONG_ARTIST_FULL_NAME, songToAdd.getSongArtistFullName()); 
 		data.put(Song.KEY_SONG_ALBUM, songToAdd.getSongAlbum()); 
 		data.put("songAmountFavourites", String.valueOf(songToAdd.getSongAmountFavourites())); 
-		data.put("id", insertedSong.getId()); 
+		data.put("id", insertedSong.getId()); //Gets the id from inserted song
+		//Status OK since we added
 		DbQueryStatus status = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+		//Set status objects data field to send in response body
 		status.setData(data);
 		return status; 
 		
 	}
 
+	/**
+	 * Retrieves song that matches songId and gets its property data 
+	 * @param songId - the string that represents the hex ID of the target song
+	 * @return DbQueryStatus object that specifies the status of the operation 
+     * and this object's data field may be set if the operation has response data
+	 */
 	@Override
 	public DbQueryStatus findSongById(String songId) {
 		// TODO Auto-generated method stub
+	  //Criteria to check if song with the id exist
 	  Criteria criteira = Criteria.where("_id").is(songId);
 	  Query query = new Query(criteira); 
+	  //Actual check to see if song exists or not
       if (!this.db.exists(query, Song.class)) {
+        //Song doesn't exist so return with not found error status
         return new DbQueryStatus("Song not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND); 
       }
+      //The song that matches the id
       Song foundSong = this.db.findOne(query, Song.class); 
+      //Data that is a map to send with body that contains all song properties 
       Map<String, String> data = new HashMap<>(); 
       data.put(Song.KEY_SONG_NAME, foundSong.getSongName()); 
       data.put(Song.KEY_SONG_ARTIST_FULL_NAME, foundSong.getSongArtistFullName()); 
       data.put(Song.KEY_SONG_ALBUM, foundSong.getSongAlbum()); 
       data.put("songAmountFavourites", String.valueOf(foundSong.getSongAmountFavourites())); 
       data.put("id", foundSong.getId()); 
+      //OK status
       DbQueryStatus status = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+      //set the song properties map as status objects data field 
       status.setData(data);
       return status; 
       
 	}
 
+	/**
+	 * Gets the song's name for the song that matches songId
+	 * @param songId - the string that represents the hex ID of the target song
+	 * @return DbQueryStatus object that specifies the status of the operation 
+     * and this object's data field may be set if the operation has response data
+	 */
 	@Override
 	public DbQueryStatus getSongTitleById(String songId) {
 		// TODO Auto-generated method stub
+	  //Criteria to check if song with the id exist
 	  Criteria criteira = Criteria.where("_id").is(songId);
+	  //Actual check to see if song exists or not
       Query query = new Query(criteira); 
       if (!this.db.exists(query, Song.class)) {
+        //Song doesn't exist so return with not found error status
         return new DbQueryStatus("Song not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND); 
       }
+      //Song that matches the id
       Song foundSong = this.db.findOne(query, Song.class);
+      //Data to send back is string containing title
       String data = foundSong.getSongName(); 
       DbQueryStatus status = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+      //Set status objects data field to the data string for name to send in response body
       status.setData(data);
       return status; 
       
 	}
 
+	/**
+	 * Deletes the song that matches the songId from mongo database
+	 * @param songId - the string that represents the hex ID of the target song
+	 * @return DbQueryStatus object that specifies the status of the operation 
+     * and this object's data field may be set if the operation has response data
+	 */
 	@Override
 	public DbQueryStatus deleteSongById(String songId) {
 		// TODO Auto-generated method stub
+	  //Criteria to check if song with the id exist
 	  Criteria criteira = Criteria.where("_id").is(songId);
       Query query = new Query(criteira); 
+      //Actual check to see if song exists or not
       if (!this.db.exists(query, Song.class)) {
+        //Song doesn't exist so return with not found error status
         return new DbQueryStatus("Song not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND); 
       }
+      //Result of the delete operation 
       DeleteResult deleteResult = this.db.remove(query, Song.class);
       if(deleteResult.getDeletedCount() <= 0) {
+        //Nothing got deleted so return error status
         return new DbQueryStatus("Nothing deleted", DbQueryExecResult.QUERY_ERROR_GENERIC);
       }
+      //OK status
       return new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
 	}
 
+	/**
+	 * Updates the song that matches songId by incrementing or decrementing coutner by 1
+	 * @param songId - the string that represents the hex ID of the target song
+	 * @param shouldDecrement - true to specify decrement by 1; false to increment by 1
+	 * @return DbQueryStatus object that specifies the status of the operation 
+     * and this object's data field may be set if the operation has response data
+	 */
 	@Override
 	public DbQueryStatus updateSongFavouritesCount(String songId, boolean shouldDecrement) {
 		// TODO Auto-generated method stub
+	  //Criteria to check if song with the id exist
 	  Criteria criteira = Criteria.where("_id").is(songId);
       Query query = new Query(criteira); 
+      //Actual check to see if song exists or not
       if (!this.db.exists(query, Song.class)) {
+        //Song doesn't exist so return with not found error status
         return new DbQueryStatus("Song not found", DbQueryExecResult.QUERY_ERROR_NOT_FOUND); 
       }
+      //Update object to specify how to update song document in database
       Update update = new Update(); 
       if(shouldDecrement) {
+        //User wants to reduce likes 
+        //First we find song
         DbQueryStatus stat = findSongById(songId);
+        //Prevent the like counter to drop below 0, error status is attempt is made
         if(Long.parseLong(((Map<String, String>)stat.getData()).get("songAmountFavourites")) <= 0) {
           return new DbQueryStatus("Can't have negative likes", DbQueryExecResult.QUERY_ERROR_GENERIC);  
         }
+        //decrement by 1
         update.inc("songAmountFavourites", -1); 
       } else {
+        //Increment by 1
         update.inc("songAmountFavourites");
       }
+      //Result of update operation 
       UpdateResult updateResult = this.db.updateFirst(query, update, Song.class); 
+      //If nothing updated give error status
       if(updateResult.getModifiedCount() <= 0) {
         return new DbQueryStatus("Nothing updated", DbQueryExecResult.QUERY_ERROR_GENERIC);
       }
+      //OK status
       return new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
 	}
 }
